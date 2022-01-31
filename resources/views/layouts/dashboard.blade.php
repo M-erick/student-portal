bbffb768fea896cb5d383b134d3e0fbf89ed5a43
eric <61746940+M-erick@users.noreply.github.com>
2022-01-31 22:38:47 +0300

    Set  dashboard design a clone of maseno portal

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,11 +1,11 @@
 <div class="grid pb-10 mt-4 ">
 
     <!-- Start Content-->
-    <div class="mb-2">
+    {{-- <div class="mb-2">
         <p class="text-lg font-semibold text-gray-400">Invoices</p>
-    </div>
-    <div class="grid grid-cols-12 gap-6 pb-5 border-b-2">
-        <div class="col-span-12 sm:col-span-12 md:col-span-8 lg:col-span-8 xxl:col-span-8">
+    </div> --}}
+    {{-- <div class="grid grid-cols-12 gap-6 pb-5 border-b-2"> --}}
+    {{-- <div class="col-span-12 sm:col-span-12 md:col-span-8 lg:col-span-8 xxl:col-span-8">
             <div class="grid grid-cols-1 gap-2 mt-3 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4">
                 <div class="p-4">
                     <p class="text-xl font-bold">RM 45,941</p>
@@ -24,8 +24,8 @@
                     <p class="text-xs font-semibold text-gray-400">Paid Today</p>
                 </div>
             </div>
-        </div>
-        <div class="col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 xxl:col-span-4">
+        </div> --}}
+    {{-- <div class="col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-4 xxl:col-span-4">
             <div class="p-4">
                 <p class="text-sm text-gray-400">Outstanding Revenue</p>
                 <div class="w-full mt-2 bg-gray-100 shadow">
@@ -34,8 +34,8 @@
                 </div>
                 <p class="mt-2 text-xs font-semibold text-gray-400">RM 45,941 Overdue</p>
             </div>
-        </div>
-    </div>
+        </div> --}}
+    {{-- </div> --}}
     <div class="grid grid-cols-1 gap-2 mt-3 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
         <div class="relative w-full overflow-hidden transition duration-300 ease-in-out bg-center bg-cover rounded-lg shadow-lg h-52 group"
             style="background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f868ecef-4b4a-4ddf-8239-83b2568b3a6b/de7hhu3-3eae646a-9b2e-4e42-84a4-532bff43f397.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y4NjhlY2VmLTRiNGEtNGRkZi04MjM5LTgzYjI1NjhiM2E2YlwvZGU3aGh1My0zZWFlNjQ2YS05YjJlLTRlNDItODRhNC01MzJiZmY0M2YzOTcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.R0h-BS0osJSrsb1iws4-KE43bUXHMFvu5PvNfoaoi8o');">
@@ -44,12 +44,12 @@
             <div class="relative flex items-center justify-center w-full h-full px-4 sm:px-6 lg:px-4">
                 <div>
                     <h3 class="text-lg text-center text-white">
-                        Total Balance
+                        Total Billed
                     </h3>
                     <h3 class="mt-2 text-3xl font-bold text-center text-white">
-                        RM 27,580
+                        Ksh 227,580.00
                     </h3>
-                    <div class="flex mt-4 space-x-4">
+                    {{-- <div class="flex mt-4 space-x-4">
                         <button
                             class="block px-4 py-3 mx-auto text-xs font-bold text-white text-indigo-600 uppercase bg-white rounded shadow focus:shadow-outline focus:outline-none">
                             Transfer
@@ -58,10 +58,11 @@
                             class="block px-4 py-3 mx-auto text-xs font-bold text-white uppercase bg-indigo-800 rounded shadow hover:bg-indigo-700 focus:shadow-outline focus:outline-none">
                             Request
                         </button>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
+
         <div class="relative w-full overflow-hidden transition duration-300 ease-in-out bg-center bg-cover rounded-lg shadow-lg h-52 group"
             style="background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f868ecef-4b4a-4ddf-8239-83b2568b3a6b/de7hhu3-3eae646a-9b2e-4e42-84a4-532bff43f397.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y4NjhlY2VmLTRiNGEtNGRkZi04MjM5LTgzYjI1NjhiM2E2YlwvZGU3aGh1My0zZWFlNjQ2YS05YjJlLTRlNDItODRhNC01MzJiZmY0M2YzOTcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.R0h-BS0osJSrsb1iws4-KE43bUXHMFvu5PvNfoaoi8o');">
             <div class="absolute inset-0 transition duration-300 ease-in-out bg-yellow-600 bg-opacity-75">
@@ -72,14 +73,14 @@
                         <div class="flex items-center p-2 bg-white rounded-md">
                             <i class="text-yellow-300 fas fa-toggle-off fa-sm"></i>
                         </div>
-                        <p>Finished Appt</p>
+                        <p>Balance</p>
                     </div>
                     <h3 class="mt-2 text-3xl font-bold text-white">
-                        120
+                        Ksh 136,794.00
                     </h3>
-                    <h3 class="mt-2 text-lg text-white text-yellow-100 ">
+                    {{-- <h3 class="mt-2 text-lg text-white text-yellow-100 ">
                         4 not confirmed
-                    </h3>
+                    </h3> --}}
                 </div>
             </div>
         </div>
@@ -87,24 +88,28 @@
             style="background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f868ecef-4b4a-4ddf-8239-83b2568b3a6b/de7hhu3-3eae646a-9b2e-4e42-84a4-532bff43f397.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Y4NjhlY2VmLTRiNGEtNGRkZi04MjM5LTgzYjI1NjhiM2E2YlwvZGU3aGh1My0zZWFlNjQ2YS05YjJlLTRlNDItODRhNC01MzJiZmY0M2YzOTcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.R0h-BS0osJSrsb1iws4-KE43bUXHMFvu5PvNfoaoi8o');">
             <div class="absolute inset-0 transition duration-300 ease-in-out bg-blue-900 bg-opacity-75">
             </div>
+
             <div class="relative flex items-center w-full h-full px-4 sm:px-6 lg:px-4">
                 <div>
                     <div class="flex items-center space-x-2 text-lg text-white">
                         <div class="flex items-center p-2 bg-white rounded-md">
                             <i class="text-blue-800 fas fa-clipboard-check fa-sm"></i>
                         </div>
-                        <p>Finished Appt</p>
+                        <p>Total Paid</p>
                     </div>
                     <h3 class="mt-2 text-3xl font-bold text-white">
+                        Ksh 90,786.00
+                    </h3>
+                    {{-- <h3 class="mt-2 text-3xl font-bold text-white">
                         72
-                    </h3>
-                    <h3 class="mt-2 text-lg text-white ">
+                    </h3> --}}
+                    {{-- <h3 class="mt-2 text-lg text-white ">
                         3.4% <span class='font-semibold text-blue-200'>vs last month</span>
-                    </h3>
+                    </h3> --}}
                 </div>
             </div>
         </div>
-        <x-userdetailscard/>
+        <x-userdetailscard />
     </div>
 
     <!-- End Content-->
