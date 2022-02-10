8f78c0f036b4657c105b2f03ea8e5d5fefd56f40
eric <61746940+M-erick@users.noreply.github.com>
2022-02-11 01:05:23 +0300

    handles academic year registration

---
+++ app/Http/Controllers/RegisterYearController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RegisterYear;
+use Illuminate\Http\Request;
+
+class RegisterYearController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('layouts.yearRegistration');
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request ,[
+            'course' =>'required',
+            'year' =>'required',
+            'semester' =>'required'
+
+        ]);
+
+        // $post = new RegisterYear();
+        // $post->admission = auth()->user()->admission ;
+        $post = RegisterYear::create([
+
+            'admission' => auth()->user()->admission,
+            'course' =>$request->input('course'),
+            'year' =>$request->input('year'),
+            'semester' =>$request->input('semester')
+
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
