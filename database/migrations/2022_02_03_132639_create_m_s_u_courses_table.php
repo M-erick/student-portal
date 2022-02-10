eb1d767a753aa840a7e8d86f6a173133ecbb88ce
eric <61746940+M-erick@users.noreply.github.com>
2022-02-10 23:36:52 +0300

    course table migration

---
+++ database/migrations/2022_02_03_132639_create_m_s_u_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMSUCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_s_u_courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('course_id')->unique();
+            $table->unsignedBigInteger('school_id');
+
+            $table->foreign('school_id')
+            ->references('school_id')
+            ->on('schools')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_s_u_courses');
+    }
+}
