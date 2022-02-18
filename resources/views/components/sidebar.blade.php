25c4e7acbe849d986837a5f93c6b1be5792a87dc
eric <61746940+M-erick@users.noreply.github.com>
2022-02-19 00:03:38 +0300

    sidebar component to be used on all pages

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,44 @@
+    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
+
+    <aside class="relative z-20 flex-shrink-0 w-20 px-2 overflow-y-auto bg-indigo-600 sm:block">
+        <div class="mb-6">
+
+            <!--Start logo -->
+            {{-- replace the image with student image --}}
+
+            <div class="flex justify-center">
+                <div class="mt-2 bg-gray-300 border-2 border-white rounded-full w-14 h-14">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVxhAxJ4D7MOeTTj6kR9PBeZonW5HM7giKjTbEmR-HMBwf3G1VqGnlwpO1kWrdyIZu8_U&usqp=CAU"
+                        class="w-auto rounded-full" />
+                </div>
+            </div>
+            <!--End logo -->
+            <!--Start NavItem -->
+
+            <div>
+                <ul class="px-4 mt-6 leading-10">
+                    <li>
+
+                        <a class="flex items-center justify-center p-2 mx-auto mb-3 cursor-pointer hover:underline"
+                            href="/registration">Register</a>
+                    </li>
+                    <li>
+                        <a class="flex items-center justify-center p-2 mx-auto mb-3 cursor-pointer hover:underline"
+                            href="/registration">RESULTS</a>
+
+
+                    </li>
+                    <li>
+                        <a class="flex items-center justify-center p-2 mx-auto mb-3 cursor-pointer hover:underline"
+                            href="/registration">REGISTER SEMESTER</a>
+
+
+                    </li>
+
+                </ul>
+            </div>
+
+            <!--End NavItem -->
+        </div>
+    </aside>
+
