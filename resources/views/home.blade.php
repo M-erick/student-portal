8660948001fbefa546d28b7c2f125eb4c4e91de5
eric <61746940+M-erick@users.noreply.github.com>
2022-02-18 22:08:17 +0300

    removed component user-dettails

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,11 +45,6 @@
 
                                 </li>
 
-                                <li
-                                    class="flex items-center justify-center p-2 mb-3 bg-yellow-400 rounded-md cursor-pointer">
-                                    <i class="text-white fas fa-headphones fa-sm"></i>
-                                </li>
-
                             </ul>
                         </div>
 
@@ -133,14 +128,9 @@
                                     </div>
                                 </div>
 
-                                {{-- <x-userdetailscard /> --}}
 
                                 <div>
-                                    <!-- He who is contented is rich. - Laozi -->
 
-
-                                    <!-- component -->
-                                    <!-- This is an example component -->
                                     <div class="max-w-2xl mx-auto mt-5">
 
                                         <div
@@ -154,73 +144,71 @@
                                             <div class="flow-root ">
                                                 <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
                                                     @foreach ($students as $student)
+                                                        <li class="py-3 sm:py-4">
+                                                            <div class="flex items-center space-x-4">
 
+                                                                <div class="flex-1 min-w-0">
+                                                                    <p
+                                                                        class="text-sm font-bold text-gray-900 truncate dark:text-white">
+                                                                        Admission No
+                                                                    </p>
 
-                                                    <li class="py-3 sm:py-4">
-                                                        <div class="flex items-center space-x-4">
-
-                                                            <div class="flex-1 min-w-0">
-                                                                <p
-                                                                    class="text-sm font-bold text-gray-900 truncate dark:text-white">
-                                                                    Admission No
-                                                                </p>
-
+                                                                </div>
+                                                                <div
+                                                                    class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                                                    {{ $student->admission }}
+                                                                </div>
                                                             </div>
-                                                            <div
-                                                                class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                                                {{ $student->admission }}
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    <li class="py-3 sm:py-4">
-                                                        <div class="flex items-center space-x-4">
+                                                        </li>
+                                                        <li class="py-3 sm:py-4">
+                                                            <div class="flex items-center space-x-4">
 
-                                                            <div class="flex-1 min-w-0">
-                                                                <p
-                                                                    class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                                    FULL NAME:
-                                                                </p>
+                                                                <div class="flex-1 min-w-0">
+                                                                    <p
+                                                                        class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                                                        FULL NAME:
+                                                                    </p>
 
+                                                                </div>
+                                                                <div
+                                                                    class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                                                    {{ $student->name }}
+                                                                </div>
                                                             </div>
-                                                            <div
-                                                                class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                                                {{ $student->name }}
-                                                            </div>
-                                                        </div>
-                                                    </li>
+                                                        </li>
 
-                                                    <li class="py-3 sm:py-4">
-                                                        <div class="flex items-center space-x-4">
+                                                        <li class="py-3 sm:py-4">
+                                                            <div class="flex items-center space-x-4">
 
-                                                            <div class="flex-1 min-w-0">
-                                                                <p
-                                                                    class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                                    University Email :
-                                                                </p>
+                                                                <div class="flex-1 min-w-0">
+                                                                    <p
+                                                                        class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                                                        University Email :
+                                                                    </p>
 
+                                                                </div>
+                                                                <div
+                                                                    class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                                                    {{ $student->email }}
+                                                                </div>
                                                             </div>
-                                                            <div
-                                                                class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                                                {{ $student->email }}
-                                                            </div>
-                                                        </div>
-                                                    </li>
-                                                    <li class="pt-3 pb-0 sm:pt-4">
-                                                        <div class="flex items-center space-x-4">
+                                                        </li>
+                                                        <li class="pt-3 pb-0 sm:pt-4">
+                                                            <div class="flex items-center space-x-4">
 
-                                                            <div class="flex-1 min-w-0">
-                                                                <p
-                                                                    class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                                    University Email Password
-                                                                </p>
+                                                                <div class="flex-1 min-w-0">
+                                                                    <p
+                                                                        class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                                                        University Email Password
+                                                                    </p>
 
+                                                                </div>
+                                                                <div
+                                                                    class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                                                    MaseNO12Kh
+                                                                </div>
                                                             </div>
-                                                            <div
-                                                                class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                                                MaseNO12Kh
-                                                            </div>
-                                                        </div>
-                                                    </li>
+                                                        </li>
                                                     @endforeach
                                                 </ul>
                                             </div>
@@ -235,6 +223,8 @@
                                     <!-- End Content-->
                                 </div>
 
+                            </div>
+                        </div>
                     </main>
 
                 </div>
